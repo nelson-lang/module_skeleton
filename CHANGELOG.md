3c2c7fb90d114748d55ed4b96af41c33e7e182d7
Allan CORNET <nelson.numerical.computation@gmail.com>
2021-05-18 10:40:52 +0200

    use validators functions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,11 @@
-# Next
+# 1.1.0 (UNRELEASED)
 
-- remove Evaluator reference (breaking change) available in Nelson 0.4.4
-- appveyor CI
-- CHANGELOG.md added
+- uses validators functions,
+- comments moved from '//' to '%',
+- remove Evaluator reference (breaking change) available in Nelson 0.4.4,
+- appveyor CI,
+- CHANGELOG.md added,
 - module-lock.json added to .gitignore
-
 
 # 1.0.0
 
--- builtin/cpp/cpp_sumBuiltin.cpp
+++ builtin/cpp/cpp_sumBuiltin.cpp
@@ -6,23 +6,21 @@
 #include "cpp_sumBuiltin.hpp"
 #include "cpp_sum.hpp"
 #include "Error.hpp"
+#include "Validators.hpp"
 //=============================================================================
 ArrayOfVector cpp_sumBuiltin(int nLhs, const ArrayOfVector& argIn)
 {
     ArrayOfVector retval;
-    if (argIn.size() != 2)
-    {
-        Error(ERROR_WRONG_NUMBERS_INPUT_ARGS);
-    }
-    if (nLhs > 1)
-    {
-        Error(ERROR_WRONG_NUMBERS_OUTPUT_ARGS);
-    }
+    nargincheck(argIn, 2, 2);
+    nargoutcheck(nLhs, 0, 1);
+    mustBeScalarOrEmpty(argIn, 0);
+    mustBeFloat(argIn, 0);
+    mustBeScalarOrEmpty(argIn, 1);
+    mustBeFloat(argIn, 1);
     ArrayOf param1 = argIn[0];
     ArrayOf param2 = argIn[1];
-    retval.push_back(
-        ArrayOf::doubleConstructor(
-            cpp_sum(param1.getContentAsDoubleScalar(), param2.getContentAsDoubleScalar())));
+    retval << ArrayOf::doubleConstructor(
+            cpp_sum(param1.getContentAsDoubleScalar(), param2.getContentAsDoubleScalar()));
     return retval;
 }
 //=============================================================================
